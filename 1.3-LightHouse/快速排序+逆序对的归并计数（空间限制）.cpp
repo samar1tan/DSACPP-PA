0aa567c7b27d22906325b51eca07488d97d4e1ff
Samaritan <30967204+samar1tan@users.noreply.github.com>
2018-08-28 00:42:05 +0800

    向量

--- 1.3-LightHouse/快速排序+逆序对的归并计数（空间限制）.cpp
+++ 1.3-LightHouse/快速排序+逆序对的归并计数（空间限制）.cpp
@@ -1,16 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
 
-const int SZ = 1 << 20;
-struct fastio {
-    char inbuf[SZ];
-    char outbuf[SZ];
-    fastio() {
-        setvbuf(stdin, inbuf, _IOFBF, SZ);
-        setvbuf(stdout, outbuf, _IOFBF, SZ);
-    }
-} io;
-
 struct point {
     int x;
     int y;
