2481d548e81869d7b4a82985918d5809fd7fbea4
Samaritan13 <zjz42027@gmail.com>
2019-05-19 08:24:37 +0800

    refactor directories

---
+++ THU-DSACPP-PA/1.3-LightHouse/快速排序+逆序对的归并计数（空间限制）.cpp
@@ -0,0 +1,111 @@
+#include <cstdio>
+#include <cstdlib>
+
+struct point {
+    int x;
+    int y;
+};
+
+int partition(point* pnt, int lo, int hi) {
+    // point cnd = pnt[lo]; // 不要对基本类型的赋值与等号习以为常，自定义类型不一定有构造函数，也不一定重载了等号
+    point cnd; // 候选轴点的备份
+    cnd.x = pnt[lo].x;
+    cnd.y = pnt[lo].y;
+
+    /* 一达到lo==hi，循环就不再进行实际操作 */
+    while (lo < hi) {
+        // while(lo < hi && cnd.x < pnt[hi].x) // 为了准确还原算法思路中的分支走向，谨慎使用复合跳出条件
+        while (lo < hi) {
+            if (cnd.x < pnt[hi].x)
+                hi--;
+            else {
+                // pnt[lo++] = pnt[hi]; // 同第62行之理
+                pnt[lo].x = pnt[hi].x;
+                pnt[lo++].y = pnt[hi].y;
+                break;
+            }
+        }
+
+        while (lo < hi) {
+            // if (pnt[hi].x < cnd.x) // 对称还是相同？
+            if(pnt[lo].x < cnd.x)
+                lo++;
+            else {
+                pnt[hi].x = pnt[lo].x;
+                pnt[hi--].y = pnt[lo].y;
+                break;
+            }
+        }
+    }
+
+    pnt[lo].x = cnd.x;
+    pnt[lo].y = cnd.y;
+    return lo;
+}
+
+void quickSort(point* pnt, int lo, int hi) {
+    if (hi - lo < 2)
+        return;
+    else {
+        int pivot = partition(pnt, lo, hi - 1); // 在[lo, hi-1]内选取轴点
+        quickSort(pnt, lo, pivot);
+        quickSort(pnt, pivot + 1, hi); // 空开轴点，需要加一
+    }
+}
+
+/* 改造了归并排序算法，在排序的同时计算顺序对数目
+ * 参考资料：“习题辅导：LightHouse”
+ */
+long int ordBetween(int* vec, int lo, int mi, int hi) { // 预防数据上溢
+    int* A = vec + lo;
+    int lb = mi - lo;
+    int B[lb]; // GCC允许（MSVC不允许）在栈上声明动态数组（可能导致Stack Overflow，后果自负）。可用于规避OJ的（堆）内存限制。
+    for (int i = 0; i < lb; i++)
+        B[i] = A[i];
+    int* C = vec + mi;
+    int lc = hi - mi;
+
+    long int count = 0;
+    for (int i = 0, j = 0, k = 0; j < lb || k < lc; ) {
+        if (j < lb && (k >= lc || B[j] < C[k])) { // 不会相等 
+            A[i++] = B[j++];
+            count += (lc - k);
+        }
+        if (k < lc && (j >= lb || C[k] < B[j]))
+            A[i++] = C[k++];
+        // count += (lb - j); // 虽然相对大小不定，但是与其计算逆序对再换算，不如直接计算顺序对
+    }
+
+    return count;
+}
+
+long int ord(int* vec, int lo, int hi) {
+    if (hi - lo < 2)
+        return 0;
+    else {
+        int mi = (lo + hi) >> 1;
+        long int lord = ord(vec, lo, mi); // 预防数据上溢
+        long int rord = ord(vec, mi, hi);
+        long int order = lord + rord + ordBetween(vec, lo, mi, hi);
+        return order;
+    }
+}
+
+int main() {
+    int n; // 灯塔总数
+    scanf("%d", &n);
+    point* points = new point[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d %d", &(points[i].x), &(points[i].y));
+
+    quickSort(points, 0, n);
+    int* ys = new int[n];
+    for(int i = 0; i < n; i++)
+    	ys[i] = points[i].y;
+    delete[] points; // 及时回收内存
+
+    long int lighted = ord(ys, 0, n); // 除了结果，每个中间变量也需要预防数据溢出
+    printf("%ld\n", lighted);
+
+    return 0;
+}
---
+++ THU-DSACPP-PA/2.2-ProperRebuild/模拟真二叉树重建+中序递归遍历.cpp
@@ -0,0 +1,47 @@
+/* 参考了：https://github.com/xtaq/algorithms/blob/master/binarytree.cpp */
+
+#include <cstdio>
+
+#define MAX 4000001 // 输入限制400000，+1即可
+
+int pre[MAX];
+int post[MAX];
+int in[MAX]; 
+int n; // 加快速度，简化符号，将精力集中在算法实现上
+
+void convert(int l1, int r1, int l2, int r2) { // 边界是序列的基本参数；pre = [l1, r1], post = [l2, r2]
+	if (l1 == r1) {
+		printf("%d ", pre[l1]); // 核心约定为pre，而不是post
+		return;
+	}
+	else {
+		int i;
+		for (i = l2; pre[l1 + 1] != post[i]; i++); // 在post中找左子树根节点
+		// 想清楚post的作用：为pre的划分找到边界
+
+		/********** 如何加快实现算法的速度？ **********
+		 * 1. 不变性：明确划分子问题；                *
+		 * 2. 单调性：准确修正参数；                  *
+		 * 3. 快速迭代：先满足基本要求，再考虑优化     *
+		 * 4. 盲目调试不可取                         *
+		 ********************************************/ 
+
+		/* 按中序减而治之：左子树 -> 根节点 -> 右子树 */
+		convert(l1 + 1, l1 + 1 + i - l2, l2, i); // l1加一，去掉原根节点；左子树长度是i - l2 + 1
+		printf("%d ", pre[l1]);
+		convert(l1 + 2 + i - l2, r1, i + 1, r2 - 1); // 右子树在pre上位于左子树右侧
+	}
+}
+
+int main() {
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+		scanf("%d", pre + i);
+	for (int i = 0; i < n; i++)
+		scanf("%d", post + i);
+
+	convert(0, n - 1, 0, n - 1); // 边界值
+	
+	return 0;
+}
